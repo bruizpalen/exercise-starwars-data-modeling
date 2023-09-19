bf90296ac1a24108012ab8307cadd99a667948b9
bruizpalen <115789234+bruizpalen@users.noreply.github.com>
2023-09-19 20:30:14 +0000

    First version

--- src/models.py
+++ src/models.py
@@ -7,6 +7,18 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=True, unique=True)
+
+    fav_vehicules = relationship('Vehicles', secondary='favorite_vehicles',
+                                 lazy='subquery', backref='users')
+    fav_people = relationship('People', secondary='favorite_people',
+                                 lazy='subquery', backref='users')
+    fav_planets = relationship('Planets', secondary='favorite_planets',
+                                 lazy='subquery', backref='users')
+
 class People(Base):
     __tablename__ = 'people'
     id = Column(Integer, primary_key=True)
@@ -15,41 +27,49 @@ class People(Base):
     hair_color = Column(String(250), nullable=False)
     eye_color = Column(String(250), nullable=False)
 
+    users = relationship('Users', secondary='favorite_people',
+                          lazy='subquery', backref='fav_people')
+
 class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     population = Column(Integer, nullable =False)
     terrain = Column(Integer, nullable=False)
-    # post_code = Column(String(250), nullable=False)
-    # person_id = Column(Integer, ForeignKey('person.id'))
-    # person = relationship(Person)
 
+    users = relationship('Users', secondary='favorite_planets',
+                          lazy='subquery', backref='fav_planets')
+    
 class Vehicles(Base):
     __tablename__ = 'vehicles'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     capacity = Column(Integer, nullable=False)
 
-
-class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
+    users = relationship('Users', secondary='favorite_vehicles',
+                          lazy='subquery', backref='fav_vehicles')
+    
 
 class FavoritePeople(Base):
     __tablename__ = 'favorite_people'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
     people_id = Column(Integer, ForeignKey('people.id'))
+
 
 class FavoriteVehicles(Base):
     __tablename__ = 'favorite_vehicles'
     id = Column (Integer, primary_key=True)
     vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    
+    
 
 class FavoritePlanets(Base):
     __tablename__ = 'favorite_planets'
     id = Column(Integer, primary_key=True)
     planet_id= Column(Integer, ForeignKey('planets.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
 
 
     def to_dict(self):
